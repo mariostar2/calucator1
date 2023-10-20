7363e12aa5c927a613deb81fa032ce294c40e24e
mariostar2 <alskfl7777@naver.com>
2023-10-20 11:24:55 +0900

    계산기 테스트 v1

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+
+                children: [
+                  Expanded(child: Container(child: Center(child: Text("1")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("2")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("3")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("CA")),color: Colors.redAccent,)),
+                ],
+              ),
+            ),
+
+
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Container(child: Center(child: Text("4")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("5")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("6")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("+")),color: Colors.yellow,)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(child: Container(child: Center(child: Text("7")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("8")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("9")),color: Colors.cyanAccent,)),
+                  Expanded(child: Container(child: Center(child: Text("0")),color: Colors.cyanAccent,)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
